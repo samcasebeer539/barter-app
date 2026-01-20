44c11584ea950a7730fba5d66f4792e7729f450e
samcasebeer539 <samuel.casebeer@gmail.com>
2026-01-19 23:58:07 -0800

    fixed profile routing issue

--- app/profile.tsx
+++ app/profile.tsx
@@ -8,7 +8,7 @@ import {
   Animated,
 } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import PostCard from '../components/PostCard';
+import PostCard from '@/components/PostCard';
 import PostCardWithDeck from '../components/PostCardWithDeck';
 
 const POSTS = [

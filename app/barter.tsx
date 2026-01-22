8e41a0ae1176af74205f01ec8b6de5c432f856c1
samcasebeer539 <samuel.casebeer@gmail.com>
2026-01-22 00:19:37 -0800

    Update barter page with new card images

--- app/barter.tsx
+++ app/barter.tsx
@@ -14,24 +14,20 @@ export default function BarterScreen() {
 
   const sampleCards = [
     {
-      title: 'Accept/Decline',
-      photo: 'https://images.unsplash.com/photo-1485965120184-e220f721d03e?w=800',
+      title: 'Accept',
+      photo: require('../assets/barter-cards/Accept.PNG'),
     },
     {
-      title: 'Counteroffer',
-      photo: 'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?w=800',
+      title: 'Decline',
+      photo: require('../assets/barter-cards/Decline.PNG'),
     },
     {
-      title: 'Question',
-      photo: 'https://images.unsplash.com/photo-1510915361894-db8b60106cb1?w=800',
+      title: 'Counter',
+      photo: require('../assets/barter-cards/Counter.PNG'),
     },
     {
-      title: 'Leather Boots',
-      photo: 'https://images.unsplash.com/photo-1542280756-74b2f55e73ab?w=800',
-    },
-    {
-      title: 'Vintage Watch',
-      photo: 'https://images.unsplash.com/photo-1523170335258-f5ed11844a49?w=800',
+      title: 'Query',
+      photo: require('../assets/barter-cards/Query.PNG'),
     },
   ];
 

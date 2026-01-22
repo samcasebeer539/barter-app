17af14db110e5277a7c5fdb9555a688637561627
samcasebeer539 <samuel.casebeer@gmail.com>
2026-01-22 00:21:27 -0800

    Fix barter card image paths using @ alias

--- app/barter.tsx
+++ app/barter.tsx
@@ -15,19 +15,19 @@ export default function BarterScreen() {
   const sampleCards = [
     {
       title: 'Accept',
-      photo: require('../assets/barter-cards/Accept.PNG'),
+      photo: require('@/assets/barter-cards/Accept.PNG'),
     },
     {
       title: 'Decline',
-      photo: require('../assets/barter-cards/Decline.PNG'),
+      photo: require('@/assets/barter-cards/Decline.PNG'),
     },
     {
       title: 'Counter',
-      photo: require('../assets/barter-cards/Counter.PNG'),
+      photo: require('@/assets/barter-cards/Counter.PNG'),
     },
     {
       title: 'Query',
-      photo: require('../assets/barter-cards/Query.PNG'),
+      photo: require('@/assets/barter-cards/Query.PNG'),
     },
   ];
 

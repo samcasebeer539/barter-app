c92df5cf5d91960218af2a0f69ab81a1f73b744c
Jay Wilson <jathwils@ucsc.edu>
2026-02-09 16:21:17 -0800

    Implemented basic login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,170 +1,187 @@
 import { Tabs } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { MaterialIcons } from '@expo/vector-icons';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { useFonts } from 'expo-font';
 import { colors } from '@/styles/globalStyles';
+import { useState, useEffect } from 'react';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 function TabBarBackground() {
-  return (
-    <View style={[StyleSheet.absoluteFill, { backgroundColor: '#000', top: 32}]} />
-  );
+    return (
+        <View style={[StyleSheet.absoluteFill, { backgroundColor: '#000', top: 32}]} />
+    );
 }
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
-    'YourFontName-Regular': require('../assets/fonts/RobotoCondensed-Regular.ttf'),
-    'YourFontName-Bold': require('../assets/fonts/RobotoCondensed-SemiBold.ttf'),
-    'YourFontName-ExtraBold': require('../assets/fonts/Oswald-Bold.ttf'),
-    // Add other font weights/styles as needed
-  });
- 
-  if (!fontsLoaded) {
-    return null;
-  }
+    const [fontsLoaded] = useFonts({
+        'YourFontName-Regular': require('../assets/fonts/RobotoCondensed-Regular.ttf'),
+        'YourFontName-Bold': require('../assets/fonts/RobotoCondensed-SemiBold.ttf'),
+        'YourFontName-ExtraBold': require('../assets/fonts/Oswald-Bold.ttf'),
+        // Add other font weights/styles as needed
+    });
+    
+    if (!fontsLoaded) {
+        return null;
+    }
 
-  return (
-    <SafeAreaProvider>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: colors.actions.offer,
-          tabBarInactiveTintColor: '#fff',
-          
-          tabBarStyle: {
-            backgroundColor: 'transparent',
-            borderTopWidth: 0,
-            height: 110,
-            paddingBottom: 42,
-            paddingTop: 20,
-            position: 'absolute',
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+
+    const onAuthStateChangedParam = (user: FirebaseAuthTypes.User | null) => {
+        console.log('onAuthStateChanged', user);
+        setUser(user);
+        if (initializing) setInitializing(false);
+    }
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChangedParam);
+        return subscriber;
+    }, []);
+
+    
+
+    return (
+        <SafeAreaProvider>
+        <Tabs
+            screenOptions={{
+            tabBarActiveTintColor: colors.actions.offer,
+            tabBarInactiveTintColor: '#fff',
             
-          },
-          
-          tabBarBackground: () => <TabBarBackground />,
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarButton: (props) => {
-            const { 
-              children, 
-              onPress, 
-              onLongPress,
-              testID,
-              accessibilityLabel,
-              accessibilityRole,
-              accessibilityState,
-              style,
-              ...rest 
-            } = props as any;
+            tabBarStyle: {
+                backgroundColor: 'transparent',
+                borderTopWidth: 0,
+                height: 110,
+                paddingBottom: 42,
+                paddingTop: 20,
+                position: 'absolute',
+                
+            },
             
-            // Determine button position based on the route
-            const href = props.href as string;
-            const isFirst = href === '/feed';
-            const isLast = href === '/profiledeck';
-            const isMiddle = href === '/barter';
+            tabBarBackground: () => <TabBarBackground />,
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarButton: (props) => {
+                const { 
+                children, 
+                onPress, 
+                onLongPress,
+                testID,
+                accessibilityLabel,
+                accessibilityRole,
+                accessibilityState,
+                style,
+                ...rest 
+                } = props as any;
+                
+                // Determine button position based on the route
+                const href = props.href as string;
+                const isFirst = href === '/feed';
+                const isLast = href === '/profiledeck';
+                const isMiddle = href === '/barter';
+                
+                return (
+                <TouchableOpacity
+                    onPress={onPress}
+                    onLongPress={onLongPress}
+                    testID={testID}
+                    accessibilityLabel={accessibilityLabel}
+                    accessibilityRole={accessibilityRole}
+                    accessibilityState={accessibilityState}
+                    style={[
+                    styles.tabButton,
+                    isFirst && styles.leftButton,
+                    isLast && styles.rightButton,
+                    isMiddle && styles.middleButton,
+                    ]}
+                >
+                    {children}
+                </TouchableOpacity>
+                );
+            },
+            }}
+        >
+            <Tabs.Screen
+            name="index"
+            options={{
+                href: null, // Hide from tab bar completely
+            }}
+            />
+            <Tabs.Screen
+            name="feed"
+            options={{
+                tabBarIcon: ({ color }) => (
+                <FontAwesome6 name="square" size={24} color={color} style={{ marginTop: 0 }}/>
+                
+                ),
+            }}
+            />
+            <Tabs.Screen
+            name="barter"
+            options={{
+                tabBarIcon: ({ color }) => (
+                <FontAwesome6 name="arrow-right-arrow-left" size={24} color={color} style={{ marginTop: 0 }}/>
+                ),
+            }}
+            />
+            <Tabs.Screen
+            name="profiledeck"
+            options={{
+                tabBarIcon: ({ color }) => (
+                <FontAwesome6 name="user-circle" size={24} color={color} style={{ marginTop: 0 }}/>
+                ),
+            }}
+            />
+    
+            <Tabs.Screen
+            name="settings"
+            options={{
+                href: null, // Hide from tab bar completely
+            }}
+            />
+            <Tabs.Screen
+            name="profile"
+            options={{
+                href: null,
+            }}
+            />
+    
             
-            return (
-              <TouchableOpacity
-                onPress={onPress}
-                onLongPress={onLongPress}
-                testID={testID}
-                accessibilityLabel={accessibilityLabel}
-                accessibilityRole={accessibilityRole}
-                accessibilityState={accessibilityState}
-                style={[
-                  styles.tabButton,
-                  isFirst && styles.leftButton,
-                  isLast && styles.rightButton,
-                  isMiddle && styles.middleButton,
-                ]}
-              >
-                {children}
-              </TouchableOpacity>
-            );
-          },
-        }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            href: null, // Hide from tab bar completely
-          }}
-        />
-        <Tabs.Screen
-          name="feed"
-          options={{
-            tabBarIcon: ({ color }) => (
-              <FontAwesome6 name="square" size={24} color={color} style={{ marginTop: 0 }}/>
-              
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="barter"
-          options={{
-            tabBarIcon: ({ color }) => (
-              <FontAwesome6 name="arrow-right-arrow-left" size={24} color={color} style={{ marginTop: 0 }}/>
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="profiledeck"
-          options={{
-            tabBarIcon: ({ color }) => (
-              <FontAwesome6 name="user-circle" size={24} color={color} style={{ marginTop: 0 }}/>
-            ),
-          }}
-        />
-  
-        <Tabs.Screen
-          name="settings"
-          options={{
-            href: null, // Hide from tab bar completely
-          }}
-        />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            href: null,
-          }}
-        />
-   
-        
-      </Tabs>
-    </SafeAreaProvider>
-  );
+        </Tabs>
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  tabButton: {
-    flex: 1,
-    backgroundColor: colors.ui.secondary,
-    marginHorizontal: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 40,
-  },
-  leftButton: {
-    borderTopLeftRadius: 2,
-    borderBottomLeftRadius: 36,
-    borderTopRightRadius: 2,
-    borderBottomRightRadius: 2,
-    marginLeft: 12,
-    top: 22,
-  },
-  middleButton: {
-    borderTopRightRadius: 2,
-    borderBottomRightRadius: 2,
-    borderTopLeftRadius: 2,
-    borderBottomLeftRadius: 2,
-    top: 22,
-  },
-  rightButton: {
-    borderTopRightRadius: 2,
-    borderBottomRightRadius: 36,
-    borderTopLeftRadius: 2,
-    borderBottomLeftRadius: 2,
-    marginRight: 12,
-    top: 22,
-  },
+    tabButton: {
+        flex: 1,
+        backgroundColor: colors.ui.secondary,
+        marginHorizontal: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 40,
+    },
+    leftButton: {
+        borderTopLeftRadius: 2,
+        borderBottomLeftRadius: 36,
+        borderTopRightRadius: 2,
+        borderBottomRightRadius: 2,
+        marginLeft: 12,
+        top: 22,
+    },
+    middleButton: {
+        borderTopRightRadius: 2,
+        borderBottomRightRadius: 2,
+        borderTopLeftRadius: 2,
+        borderBottomLeftRadius: 2,
+        top: 22,
+    },
+    rightButton: {
+        borderTopRightRadius: 2,
+        borderBottomRightRadius: 36,
+        borderTopLeftRadius: 2,
+        borderBottomLeftRadius: 2,
+        marginRight: 12,
+        top: 22,
+    },
 });
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,89 @@
 import { Redirect } from 'expo-router';
-
+import { useState } from 'react';
+import { Text, View, StyleSheet, TextInput, KeyboardAvoidingView, Button, ActivityIndicator } from 'react-native'
+import auth from '@react-native-firebase/auth'
+import { FirebaseError } from 'firebase/app'
 export default function Index() {
-  // Redirect to the feed screen as the initial screen
-  return <Redirect href="/feed" />;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const signUp = async () => {
+        setLoading(true);
+        try {
+            await auth().createUserWithEmailAndPassword(email, password);
+            alert('Check your email!');
+        } catch(e: any) {
+            const err = e as FirebaseError;
+            alert('Sign Up Failed: ' + err.message)
+        } finally {
+            setLoading(false);
+        } 
+    }
+
+    const signIn = async () => {
+        setLoading(true);
+        try {
+            await auth().signInWithEmailAndPassword(email, password);
+            alert('Check your email!');
+        } catch(e: any) {
+            const err = e as FirebaseError;
+            alert('Sign In Failed: ' + err.message)
+        } finally {
+            setLoading(false);
+        } 
+    }
+
+
+
+    // Redirect to the feed screen as the initial screen
+    // return <Redirect href="/feed" />;
+    return (
+        <View 
+          style={styles.container}
+        >
+            <KeyboardAvoidingView behavior="padding">
+                <TextInput 
+                    style={styles.input}
+                    value={email}
+                    onChangeText={setEmail}
+                    autoCapitalize="none"
+                    keyboardType='email-address'
+                    placeholder='Email'
+                />
+                <Text>Password</Text>
+                <TextInput 
+                    style={styles.input}
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
+                {loading ? (
+                    <ActivityIndicator size={"small"} style={{ margin: 28 }} />
+                ) : (
+                    <>
+                        <Button onPress={signUp} title="Sign Up" />
+                        <Button onPress={signIn} title="Sign In" />
+                    </>
+                )
+                } 
+            </KeyboardAvoidingView>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginHorizontal: 20, 
+        flex: 1,
+        justifyContent: "center",
+    },
+    input: {
+        marginVertical: 4,
+        height: 50,
+        borderWidth: 1,
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff',
+    }
+})

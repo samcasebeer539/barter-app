71b59d02d21f5baf632af020e2b4673a29d48d1d
Jay Wilson <jathwils@ucsc.edu>
2026-02-25 13:13:08 -0800

    index

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
-import { Redirect } from 'expo-router';
+import { useRouter } from 'expo-router';
 
 export default function Index() {
-  // Redirect to the feed screen as the initial screen
-  return <Redirect href="/feed" />;
+    const router = useRouter()
+    router.replace('/feed');
 }

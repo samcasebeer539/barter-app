ec679a18fb81b5ea106d2876f6211b7b2c5ba452
Jay Wilson <jathwils@ucsc.edu>
2026-07-22 14:42:40 -0700

    Updated route so it will log error message in case of server error

--- backend/routes/dev/settings/route.py
+++ backend/routes/dev/settings/route.py
@@ -2,20 +2,31 @@ from flask import jsonify, request, Blueprint
 import traceback
 from firebase_admin import auth as firebase_auth
 from backend import user_data_collection
+from helpers import get_uid_from_request
 
 settings_bp = Blueprint("settings", __name__)
 
+# Gets the status if notifications and/or locations services are enabled
 @settings_bp.route("/settings", methods=["GET"])
 def get_settings():
 
-    token = request.headers.get("Authorization").replace("Bearer ", "")
-    decoded = firebase_auth.verify_id_token(token)
+    try: 
+        uid, err = get_uid_from_request()
+        if err: return err
 
-    uid = decoded["uid"]
+        # Return all posts from all users except the current user
+        user = user_data_collection.find_one({"firebase_uid": uid})
 
-    user = user_data_collection.find_one({"firebase_uid": uid})
+        if not user:
+            return jsonify({
+                "error": "User not found"
+            }), 404
 
-    return jsonify({
-        "is_location_allowed": user.get("is_location_allowed", True),
-        "notifications_enabled": user.get("notifications_enabled", True)
-    })
+        return jsonify({
+            "is_location_allowed": user.get("is_location_allowed", True),
+            "notifications_enabled": user.get("notifications_enabled", True)
+        })
+    
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({"error": str(e)}), 500
